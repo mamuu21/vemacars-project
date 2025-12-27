eafd0203fb00343c96652cc6515c8cc403a5fe56
mamuu21 <mariamumussa071@gmail.com>
2025-12-27 04:24:45 +0300

    updates on shipfile and next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-  images: {
-    unoptimized: true,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'export',
+  images: { unoptimized: true }
 }
 
 export default nextConfig
+
